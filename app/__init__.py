b780a935afef846967a7a301029f3fae42a6008d
scad89 <scad200@gmail.com>
2022-07-28 16:52:12 +0300

    deleted comment code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-from core.db import metadata, engine
-from app.jobs.jb_models import jobs
-from app.users.us_models import users
 from .users.us_endpoint import user
 from .auth.auth_endpoint import auth
 from .jobs.jb_endpoint import job
@@ -9,7 +6,6 @@ from .jobs.jb_endpoint import job
 
 def create_app():
     app = FastAPI(title="Test Project")
-    # metadata.create_all(bind=engine)
 
     app.include_router(auth, prefix="/auth", tags=["auth"])
     app.include_router(user, prefix="/users", tags=["users"])
