718567f52594284a26cc3f9e1be0ac45739a49dd
scad89 <scad200@gmail.com>
2022-07-29 13:02:47 +0300

    fixed config

--- core/config.py
+++ core/config.py
@@ -1,15 +1,18 @@
-from distutils.command.config import config
-from starlette.config import Config
+import os
+from pydantic import BaseSettings
 
 
-config = Config('.env')
+class Config(BaseSettings):
+    DEBUG: bool
+    SECRET_KEY: str
+    SQLALCHEMY_DATABASE_URI: str
+    RESET_TOKEN: str
+    VERIFICATION_TOKEN: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str = "HS256"
+
+    class Config:
+        env_file = f"{os.getcwd()}/.env"
 
 
-class Config(object):
-    DEBUG = config("DEBUG", cast=bool)
-    SECRET_KEY = config('SECRET_KEY', cast=str)
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', cast=str)
-    RESET_TOKEN = config("RESET_TOKEN", cast=str)
-    VERIFICATION_TOKEN = config("VERIFICATION_TOKEN", cast=str)
-    ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int)
-    ALGORITHM = "HS256"
+Config = Config()
